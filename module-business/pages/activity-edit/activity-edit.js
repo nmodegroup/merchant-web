6c1f2ac842bd0ad2f4d6ecadd7b6cba37029cbfe
stone <1484542698@qq.com>
2019-08-30 00:47:10 +0800

    feat:创建活动

--- module-business/pages/activity-edit/activity-edit.js
+++ module-business/pages/activity-edit/activity-edit.js
@@ -1,8 +1,10 @@
 // module-business/pages/activity-edit/activity-edit.js
 const activityService = require('../../../service/activity');
+const commonService = require('../../../service/common');
 const { isEdit, initValue, isEmpty } = require('../../../utils/global');
 const { PageConfig } = require('../../../utils/page');
 const PageHelper = new PageConfig();
+const dateUtil = require('../../../utils/date');
 
 Page({
   /**
@@ -16,8 +18,8 @@ Page({
     theme: '', // 活动主题
     beginTime: '', // 活动开始时间
     endTime: '', // 活动结束时间
-    cityId: '', // 城市id
-    areaId: '', // 区域id
+    cityId: '', // 城市 id
+    areaId: '', // 区域 id
     cityName: '', // 所在城市名称
     areaName: '', // 所在区域名称
     address: '', // 详细地址
@@ -25,11 +27,15 @@ Page({
     guest: '', // 嘉宾信息
     quotaType: '', // 限制预订数量类型（0不限 1按系统已有桌位限制 2按固定名额限制）
     quota: '', // 预订限额数量
-    banner: '', // 活动宣传banner图url
+    banner: '', // 活动宣传 banner 图 url
     post: '', // 活动海报url
     verifyed: false, // 表单是否校验通过
-    visibleDateTime: false, // 时间选择
-    visibleArea: false // 省市区选择
+    visibleStartTime: false, // 开始时间选择
+    visibleEndTime: false, // 结束时间选择
+    visibleArea: false, // 省市区选择
+    areaList: [], // 省市区列表
+    selectArea: '', // 选择的区域
+    selectTime: '' // 选择的时间
   },
 
   /**
@@ -53,6 +59,15 @@ Page({
     });
     PageHelper.setupPageConfig(this);
     this.requestActivityInfo();
+    this.requestCityList();
+  },
+
+  requestCityList() {
+    commonService.getCityAll().then(res => {
+      this.setData({
+        areaList: res
+      });
+    });
   },
 
   /**
@@ -73,19 +88,28 @@ Page({
     });
   },
 
-  onInput(event) {
+  onStartInput(event) {
+    console.log('onStartInput', event);
+    this.setData({
+      currentDate: event.detail
+    });
+  },
+
+  onEndInput(event) {
+    console.log('onEndInput', event);
     this.setData({
       currentDate: event.detail
     });
   },
 
   /**
-   * 点击 cell 事件
+   * 点击 cell 事件，（时间、地区）
    */
   handleCellClick(event) {
     const { type } = event.currentTarget.dataset;
     const strategy = {
-      time: () => this.setVisible('visibleDateTime'),
+      beginTime: () => this.setVisible('visibleStartTime'),
+      endTime: () => this.setVisible('visibleEndTime'),
       area: () => this.setVisible('visibleArea')
     };
     strategy[type]();
@@ -94,6 +118,72 @@ Page({
   setVisible(visibleType) {
     this.setData({
       [visibleType]: true
+    });
+  },
+
+  setUnvisible(unvisibleType) {
+    this.setData({
+      [unvisibleType]: false
+    });
+  },
+
+  /**
+   * 时间确认
+   */
+  onTimeConfirm(event) {
+    const { type } = event.currentTarget.dataset;
+    console.log('type', type);
+    this.setData({
+      [type]: dateUtil.formatDate(event.detail),
+      visibleStartTime: false,
+      visibleEndTime: false
+    });
+  },
+
+  /**
+   * 时间取消
+   */
+  onTimeCancel(event) {
+    const { type } = event.currentTarget.dataset;
+    const strategy = {
+      beginTime: () => this.setUnvisible('visibleStartTime'),
+      endTime: () => this.setUnvisible('visibleEndTime')
+    };
+    strategy[type]();
+  },
+
+  /**
+   * 选择省市区确认
+   */
+  onCityConfirm(event) {
+    const selectValues = event.detail.values;
+    this.setData({
+      visibleArea: false,
+      selectArea: `${selectValues[0].name} ${selectValues[1].name} ${selectValues[2].name}`,
+      cityName: selectValues[1].name,
+      cityId: selectValues[1].code,
+      areaName: selectValues[2].name,
+      areaId: selectValues[2].code
+    });
+    this.refreshFormVerify();
+  },
+
+  /**
+   * 选择省市区取消
+   */
+  onCityCancel() {
+    this.setData({
+      visibleArea: false
+    });
+  },
+
+  /**
+   * 限制预订切换
+   */
+  onRadioChange(event) {
+    console.log(event);
+    this.setData({
+      quotaType: event.detail
     });
   },
 
--- utils/date.js
+++ utils/date.js
@@ -8,10 +8,10 @@ export function formatDate(timestamp) {
   const date = new Date(timestamp);
   const Y = date.getFullYear() + '-';
   const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  const D = date.getDate() + ' ';
-  const h = date.getHours() + ':';
-  const m = date.getMinutes() + ':';
-  const s = date.getSeconds();
+  const D = date.getDate() < 10 ? `0${date.getDate()} ` : `${date.getDate()} `;
+  const h = date.getHours() < 10 ? `0${date.getHours()}:` : `${date.getHours()}:`;
+  const m = date.getMinutes() < 10 ? `0${date.getMinutes()}:` : `${date.getMinutes()}:`;
+  const s = date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`;
   return `${Y}${M}${D}${h}${m}${s}`;
 }
 
@@ -19,7 +19,7 @@ export function formatUnixDate(timestamp) {
   const date = new Date(timestamp);
   const Y = date.getFullYear() + '-';
   const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  const D = date.getDate();
+  const D = date.getDate() < 10 ? `0${date.getDate()} ` : `${date.getDate()} `;
   return `${Y}${M}${D}`;
 }
 
