03a38a7fa5526b03b659e69d852e0b44c783416c
kgm1818 <1634750252@qq.com>
2020-01-09 23:14:33 +0800

    bug fix

--- module/pages/draw-deposit/draw-deposit.js
+++ module/pages/draw-deposit/draw-deposit.js
@@ -10,7 +10,11 @@ Page({
    */
   data: {
     enabled: false,
-    amount: 0
+    amount: 0,
+    balance: 0,
+    totalEarnAmount: 0,
+    withdrawalAmount: 0,
+    value: ""
   },
 
   /**
@@ -24,14 +28,13 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.getBalance()
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
   bindKeyInput(e){
     const { value } = e.detail
@@ -42,13 +45,41 @@ Page({
   },
   onClickBtn(){
     if (!this.data.enabled) return
-    this.toast.showToast({
-      content: "提现功能暂未开放"
-    });
-    return
+    const { amount } = this.data;
+    if (amount < 1) {
+      this.toast.showToast({
+        content: "单次提现金额不能低于1元"
+      });
+      return
+    }
+    if (amount > 5000) {
+      this.toast.showToast({
+        content: "每日提现金额不能超过5000元"
+      });
+      return
+    }
+    // this.toast.showToast({
+    //   content: "提现功能暂未开放"
+    // });
+    // return
     this.isLoadActivityFirst = true
     this.postExtract({
       amount: this.data.amount
+    })
+  },
+  /*
+* 余额信息
+*/
+  getBalance() {
+    PageHelper.requestWrapper(centerService.getBalance()).then(res => {
+      console.log(res)
+      let { balance, totalEarnAmount, withdrawalAmount } = this.data;
+      balance = res.balance;
+      totalEarnAmount = res.total;
+      withdrawalAmount = res.withdrawal;
+      this.setData({ balance, totalEarnAmount, withdrawalAmount })
+    }).catch(err => {
+      console.error(err)
     })
   },
   postExtract(params){
@@ -57,15 +88,30 @@ Page({
       this.isLoadActivityFirst
     ).then(res => {
       console.log(res)
-      this.isLoadActivityFirst = false
+      this.isLoadActivityFirst = false;
+      this.toast.showToast({
+        content: "提现成功"
+      });
+      this.setData({
+        amount: 0,
+        enabled: false,
+        value: ""
+      })
+      this.onReady()
     }).catch( err => {
-      this.isLoadActivityFirst = false
+      this.isLoadActivityFirst = false;
       if (err && err.msg) {
         this.toast.showToast({
           content: err.msg
         });
       }
     })
+  },
+  loadPage(){
+    const pages = getCurrentPages()
+    const perpage = pages[pages.length - 1]
+    console.log(perpage)
+    perpage.onLoad()  
   },
   /**
    * 生命周期函数--监听页面隐藏
